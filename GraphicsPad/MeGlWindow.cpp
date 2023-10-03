462b04d0d5bb26639ae0c9e2e2072e836ac4f3df
Chris Yuan Zhong <chrisyuanzhong@outlook.com>
2023-10-02 20:15:36 -0600

    Assignment 4 done

--- GraphicsPad/MeGlWindow.cpp
+++ GraphicsPad/MeGlWindow.cpp
@@ -14,16 +14,20 @@ const uint NUM_VERTICES_PER_TRI = 3;
 const uint NUM_FLOATS_PER_VERTICE = 6;
 const uint VERTEX_BYTE_SIZE = NUM_FLOATS_PER_VERTICE * sizeof(float);
 GLuint programID;
-GLuint numIndices;
+GLuint num3DIndices;
+GLuint num2DIndices;
 
 void sendDataToOpenGL()
 {
 	ShapeData shape = ShapeGenerator::makeCube();
+	ShapeData tri = ShapeGenerator::makeTriangle();
 
 	GLuint vertexBufferID;
 	glGenBuffers(1, &vertexBufferID);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
-	glBufferData(GL_ARRAY_BUFFER, shape.vertexBufferSize(), shape.vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, shape.vertexBufferSize() + tri.vertexBufferSize(), 0, GL_STATIC_DRAW);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, shape.vertexBufferSize(), shape.vertices);
+	glBufferSubData(GL_ARRAY_BUFFER, shape.vertexBufferSize(), tri.vertexBufferSize(), tri.vertices);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
 	glEnableVertexAttribArray(1);
@@ -32,18 +36,59 @@ void sendDataToOpenGL()
 	GLuint indexArrayBufferID;
 	glGenBuffers(1, &indexArrayBufferID);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexArrayBufferID);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, shape.indexBufferSize(), shape.indices, GL_STATIC_DRAW);
-	numIndices = shape.numIndices;
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, shape.indexBufferSize() + tri.indexBufferSize(), 0, GL_STATIC_DRAW);
+	glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, shape.indexBufferSize(), shape.indices);
+	glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, shape.indexBufferSize(), tri.indexBufferSize(), tri.indices);
+
+	num3DIndices = shape.numIndices;
+	num2DIndices = tri.numIndices;
+
 	shape.cleanup();
+	tri.cleanup();
+
+	//GLuint vertexBufferID;
+	//glGenBuffers(1, &vertexBufferID);
+	//glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+	//glBufferData(GL_ARRAY_BUFFER, shape.vertexBufferSize(), shape.vertices, GL_STATIC_DRAW);
+	//glEnableVertexAttribArray(0);
+	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+	//glEnableVertexAttribArray(1);
+	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (char*)(sizeof(float) * 3));
+
+	//GLuint indexArrayBufferID;
+	//glGenBuffers(1, &indexArrayBufferID);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexArrayBufferID);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, shape.indexBufferSize(), shape.indices, GL_STATIC_DRAW);
+	//numIndices = shape.numIndices;
+	//shape.cleanup();
 }
 
 void MeGlWindow::paintGL()
 {
+	//glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+	//glViewport(0, 0, width(), height());
+
+	//mat4 projectionMatrix = glm::perspective(70.0f, ((float)width()) / height(), 0.1f, 10.0f);
+	//mat4 projectionTranslationMatrix = glm::translate(projectionMatrix, vec3(0.0f, 0.0f, -6.0f));
+	//mat4 fullTransformMatrix = glm::rotate(projectionTranslationMatrix, 45.0f, vec3(1.0f, 1.0f, 1.0f));
+
+	//GLint fullTransformMatrixUniformLocation =
+	//	glGetUniformLocation(programID, "fullTransformMatrix");
+
+	//glUniformMatrix4fv(fullTransformMatrixUniformLocation, 1,
+	//	GL_FALSE, &fullTransformMatrix[0][0]);
+
+	//// Cube
+	//glDrawElements(GL_TRIANGLES, num3DIndices, GL_UNSIGNED_SHORT, 0);
+
+	// Rewrite the above code to draw both the cube and the triangle, using the same vertex buffer object.
+	// The triangle should be a 2D object at the upper right corner of the screen.
+
 	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 	glViewport(0, 0, width(), height());
 
 	mat4 projectionMatrix = glm::perspective(70.0f, ((float)width()) / height(), 0.1f, 10.0f);
-	mat4 projectionTranslationMatrix = glm::translate(projectionMatrix, vec3(-6.0f, 2.0f, -6.0f));
+	mat4 projectionTranslationMatrix = glm::translate(projectionMatrix, vec3(0.0f, 0.0f, -6.0f));
 	mat4 fullTransformMatrix = glm::rotate(projectionTranslationMatrix, 45.0f, vec3(1.0f, 1.0f, 1.0f));
 
 	GLint fullTransformMatrixUniformLocation =
@@ -52,35 +97,34 @@ void MeGlWindow::paintGL()
 	glUniformMatrix4fv(fullTransformMatrixUniformLocation, 1,
 		GL_FALSE, &fullTransformMatrix[0][0]);
 
-	// Cube 1
-	glDrawElements(GL_TRIANGLES, numIndices, GL_UNSIGNED_SHORT, 0);
+	// Cube
+	glDrawElements(GL_TRIANGLES, num3DIndices, GL_UNSIGNED_SHORT, 0);
+
+	// Triangle 1
+	mat4 projectionMatrix2 = glm::perspective(70.0f, ((float)width()) / height(), 0.1f, 10.0f);
+	mat4 projectionTranslationMatrix2 = glm::translate(projectionMatrix2, vec3(-4.0f, 1.0f, -4.0f));
+	mat4 fullTransformMatrix2 = glm::rotate(projectionTranslationMatrix2, 120.0f, vec3(1.0f, 1.0f, 1.0f));
 	
-	projectionTranslationMatrix = glm::translate(projectionMatrix, vec3(6.0f, 2.0f, -6.0f));
-	fullTransformMatrix = glm::rotate(projectionTranslationMatrix, 45.0f, vec3(1.0f, 1.0f, 1.0f));
+	GLint fullTransformMatrixUniformLocation2 =
+		glGetUniformLocation(programID, "fullTransformMatrix");
 
-	glUniformMatrix4fv(fullTransformMatrixUniformLocation, 1,
-		GL_FALSE, &fullTransformMatrix[0][0]);
+	glUniformMatrix4fv(fullTransformMatrixUniformLocation2, 1,
+		GL_FALSE, &fullTransformMatrix2[0][0]);
 
-	// Cube 2
-	glDrawElements(GL_TRIANGLES, numIndices, GL_UNSIGNED_SHORT, 0);
+	glDrawElements(GL_TRIANGLES, num2DIndices, GL_UNSIGNED_SHORT, (void*)(sizeof(GLushort) * num3DIndices));
 
-	projectionTranslationMatrix = glm::translate(projectionMatrix, vec3(6.0f, -2.0f, -6.0f));
-	fullTransformMatrix = glm::rotate(projectionTranslationMatrix, 45.0f, vec3(1.0f, 1.0f, 1.0f));
+	// Triangle 2
+	mat4 projectionMatrix3 = glm::perspective(70.0f, ((float)width()) / height(), 0.1f, 10.0f);
+	mat4 projectionTranslationMatrix3 = glm::translate(projectionMatrix3, vec3(4.0f, -1.0f, -4.0f));
+	mat4 fullTransformMatrix3 = glm::rotate(projectionTranslationMatrix3, 120.0f, vec3(1.0f, 1.0f, 1.0f));
 
-	glUniformMatrix4fv(fullTransformMatrixUniformLocation, 1,
-		GL_FALSE, &fullTransformMatrix[0][0]);
+	GLint fullTransformMatrixUniformLocation3 =
+		glGetUniformLocation(programID, "fullTransformMatrix");
 
-	// Cube 3
-	glDrawElements(GL_TRIANGLES, numIndices, GL_UNSIGNED_SHORT, 0);
+	glUniformMatrix4fv(fullTransformMatrixUniformLocation3, 1,
+		GL_FALSE, &fullTransformMatrix3[0][0]);
 
-	projectionTranslationMatrix = glm::translate(projectionMatrix, vec3(-6.0f, -2.0f, -6.0f));
-	fullTransformMatrix = glm::rotate(projectionTranslationMatrix, 45.0f, vec3(1.0f, 1.0f, 1.0f));
-
-	glUniformMatrix4fv(fullTransformMatrixUniformLocation, 1,
-		GL_FALSE, &fullTransformMatrix[0][0]);
-
-	// Cube 4
-	glDrawElements(GL_TRIANGLES, numIndices, GL_UNSIGNED_SHORT, 0);
+	glDrawElements(GL_TRIANGLES, num2DIndices, GL_UNSIGNED_SHORT, (void*)(sizeof(GLushort) * num3DIndices));
 }
 
 bool checkStatus(
--- GraphicsPad/Primitives/ShapeGenerator.cpp
+++ GraphicsPad/Primitives/ShapeGenerator.cpp
@@ -6,29 +6,29 @@ using glm::vec3;
 
 ShapeData ShapeGenerator::makeTriangle()
 {
-	ShapeData ret;
+    ShapeData ret;
 
-	Vertex myTri[] =
-	{
-		glm::vec3(+0.0f, +1.0f, +0.0f),
-		glm::vec3(+1.0f, +0.0f, +0.0f),
+    Vertex myTri[] =
+    {
+        glm::vec3(+0.0f, +1.0f, +0.0f),
+        glm::vec3(+1.0f, +0.0f, +0.0f),
 
-		glm::vec3(-1.0f, -1.0f, +0.0f),
-		glm::vec3(+0.0f, +1.0f, +0.0f),
+        glm::vec3(-1.0f, -1.0f, +0.0f),
+        glm::vec3(+0.0f, +1.0f, +0.0f),
 
-		glm::vec3(+1.0f, -1.0f, +0.0f),
-		glm::vec3(+0.0f, +0.0f, +1.0f),
-	};
-	ret.numVertices = NUM_ARRAY_ELEMENTS(myTri);
-	ret.vertices = new Vertex[ret.numVertices];
-	memcpy(ret.vertices, myTri, sizeof(myTri));
+        glm::vec3(+1.0f, -1.0f, +0.0f),
+        glm::vec3(+0.0f, +0.0f, +1.0f),
+    };
+    ret.numVertices = NUM_ARRAY_ELEMENTS(myTri);
+    ret.vertices = new Vertex[ret.numVertices];
+    memcpy(ret.vertices, myTri, sizeof(myTri));
 
-	GLushort indices[] = { 0, 1, 2 };
-	ret.numIndices = NUM_ARRAY_ELEMENTS(indices);
-	ret.indices = new GLushort[ret.numIndices];
-	memcpy(ret.indices, indices, sizeof(indices));
+    GLushort indices[] = { 0, 1, 2 };
+    ret.numIndices = NUM_ARRAY_ELEMENTS(indices);
+    ret.indices = new GLushort[ret.numIndices];
+    memcpy(ret.indices, indices, sizeof(indices));
 
-	return ret;
+    return ret;
 }
 
 ShapeData ShapeGenerator::makeCube() {
